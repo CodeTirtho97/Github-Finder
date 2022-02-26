22b3fba654fc76518f08fa2deb323431b578863f
Tirthoraj <tirthoraj.1997@gmail.com>
2022-02-26 21:15:15 +0530

    Added action files and used axios

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -5,18 +5,22 @@ import RepoList from "../components/repos/RepoList";
 import { useEffect, useContext } from "react";
 import { useParams } from "react-router-dom";
 import GithubContext from "../context/github/GithubContext";
+import { getUserAndRepos } from "../context/github/GithubActions";
 
 function User() {
-  const { getUser, user, loading, getUserRepos, repos } =
-    useContext(GithubContext);
+  const { user, loading, repos, dispatch } = useContext(GithubContext);
 
   const params = useParams();
 
   useEffect(() => {
-    getUser(params.login);
-    getUserRepos(params.login);
-    //eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    dispatch({ type: "SET_LOADING" });
+    const getUserData = async () => {
+      const userData = await getUserAndRepos(params.login);
+      dispatch({ type: "GET_USER_AND_REPOS", payload: userData });
+    };
+
+    getUserData();
+  }, [dispatch, params.login]);
 
   const {
     name,
@@ -26,7 +30,6 @@ function User() {
     bio,
     blog,
     twitter_username,
-    login,
     html_url,
     followers,
     following,
